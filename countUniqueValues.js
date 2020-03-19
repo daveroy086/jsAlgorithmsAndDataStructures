5ad82d03ed59a74bb85f81124daa283bd35d0c05
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-19 16:21:04 -0400

    finished  coding

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,4 +1,5 @@
 function countUniqueValues(arr){
+
     
     /*Implement a function called countUniqueValues,
     which accepts a sorted array, and counts the
@@ -7,14 +8,28 @@ function countUniqueValues(arr){
     */
 
    //initialize two pointers
+   let i = 0;
+   let j = 1;
+   let len = arr.length;
+
    //use a while loop that goes until the second pointer gets to the end of the array
-   //if the values of the array at the two pointers are the same increment the second pointer up
-   // else, if the values are not the same move the first pointer up one and change the arr value at that position to that of the second pointer
-   //when the while loop has ended get the location of the first pointer and add one to it.  This is the number of values we wanted   
+   while(j < len){
+        //if the values of the array at the two pointers are the same increment the second pointer up
+        if(arr[i] == arr[j]){
+            j += 1;
+        // else, if the values are not the same move the first pointer up one and change the arr value at that position to that of the second pointer
+        }else{
+            i += 1;
+            arr[i] = arr[j];
+        }
+   }//end while
+
+        //when the while loop has ended get the location of the first pointer and add one to it.  This is the number of values we wanted
+    return i;
 
 }//end countUniqueValues()
 
 countUniqueValues([1, 1, 1, 1, 1, 2]);//2
-countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);//7
-countUniqueValues([]);//0
-countUniqueValues([-2, -1, -1, 0, 1]);//4
+//countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);//7
+//countUniqueValues([]);//0
+//countUniqueValues([-2, -1, -1, 0, 1]);//4
