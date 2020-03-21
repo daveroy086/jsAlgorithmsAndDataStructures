414a725850d6b8c8c3728c13de6088349e8ba0f2
dave086 <51098204+daveroy086@users.noreply.github.com>
2020-03-21 12:41:50 -0400

    Create areThereDuplicates.js

---
+++ areThereDuplicates.js
@@ -0,0 +1,14 @@
+//For the areThereDuplicates file check the video: does multiple pointers give O(nlogn).
+function areThereDuplicates(a){
+
+    let myArgs = [arguments];  //does this help?
+    console.log(myArgs);
+    //return when multiplePointers first moves i
+
+}//end areThereDuplicates()
+
+
+//Test Cases:
+console.log(areThereDuplicates(1, 2, 3));//false
+console.group(areThereDuplicates(1, 2, 2)); //true
+console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
