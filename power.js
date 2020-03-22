b626156b3914c6bf80a64117a973827e038c1a1e
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-22 18:38:04 -0400

    finished power

--- power.js
+++ power.js
@@ -1,12 +1,16 @@
 function power(base, exp){
+
+    //use recursion to calculate the power of a number
     
     if(exp == 0){         //base case
         return 1;
     }
+
     return base * power(base, exp - 1);
+
 }//end power()
 
 //test cases:
-power(2, 0);//1
-power(2, 2);//4
-power(2, 4);//16
+console.log(power(2, 0));//1
+console.log(power(2, 2));//4
+console.log(power(2, 4));//16
