36f1a36f4e0a802427af67dcee4400fd20262c2f
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-23 08:18:30 -0400

    works

--- recursiveRange.js
+++ recursiveRange.js
@@ -1,5 +1,14 @@
 function recursiveRange(n){
 
-    //use recursion to add the numbers from 0 to number
+    //use recursion to add the integers from 1 to number
+    if (n <= 0) return 'out of range';
+
+    if (n == 1) return 1;
+    return n + recursiveRange(n - 1);
     
 }
+console.log(recursiveRange(-1));//out or range
+console.log(recursiveRange(0));//out of range
+console.log(recursiveRange(5));//15
+console.log(recursiveRange(10));//55
+console.log(recursiveRange(15));//120
