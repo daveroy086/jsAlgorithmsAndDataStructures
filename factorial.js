6b5ae269a4ba45c6df37aa7716053f5a3b9770e6
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-23 12:14:59 -0400

    done factorial

--- factorial.js
+++ factorial.js
@@ -2,17 +2,16 @@ function factorial(n){
 
     //use recursion to calculate the factorial of a number.
 
-    if (n == 1){    //base case
+    if (n <= 1){    //base case
         return 1;
     }
 
-    if(n > 1){
-        return n * factorial(n - 1);
+    return n * factorial(n - 1);
  
 }//end factorial()
 
 for(i = 1; i < 10; i++){
     console.log("");
     console.log("a new run");
-    console.log(factorial(i));
+    console.log('n is ', i, ' and n! is ', factorial(i));
 }
