7655ef2788bca34ab436c82ca3aeedf57f905505
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-24 10:35:10 -0400

    ready to test on web

--- reverse.js
+++ reverse.js
@@ -1,16 +1,39 @@
 function reverse(aString){
 
     //reverse becomes my recursive function wrapper
-    let reverseArray = aString...    //turn string into an array
-    let desrever = []; //an array to hold the letters as I accumulate them
 
-    function reverseInner(){
-        //get the last letter from reverse array
-        //add it to the beginning of desrever
-        //callreverseInner on the remainder of reverseArray
-    }
-    //return desrever.join(); 
-}
+    let reversedArray = []; //an array to hold the letters as I accumulate them
 
-console.log(reverse('emosewa')); //awesome
-console.log(reverse('rithmschool')); //loohcsmhtir
+    function reverseInner(aString){
+        console.log("aString is ", aString);
+
+        if(aString.length == 1){    //base case
+            reversedArray.push(aString[0]);
+            console.log("reversedArray(base) is ", reversedArray);
+            return reversedArray;
+        }
+
+        //string is ewa; shorten the string to ew
+        let aNewString = aString.substring(0, aString.length - 1);
+          //call inner on ew
+          reverseInner(aNewString);
+          //string is ew; shorten to e
+            //call inner on e
+              //this is the base case
+              //return reversedArray
+          //add the last letter of ew to the beginning of reversedArray
+          //return reversedArray
+        //add the last letter of ewa to reversedArray and return reversedArray
+        return reversedArray.unshift(aString[aString.length - 1]);
+    }//end reverseInner()
+
+    reverseInner(aString);
+
+    let myOutput = reversedArray.join("");
+    console.log(myOutput);
+    return myOutput ;
+        
+}//end reverse()
+
+console.log(reverse('ewa')); //awesome
+//console.log(reverse('rithmschool')); //loohcsmhtir
