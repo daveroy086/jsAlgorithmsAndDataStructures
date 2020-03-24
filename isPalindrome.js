948cb65a6c8a722492a3dba7fa36ab45975ac65f
dave086 <51098204+daveroy086@users.noreply.github.com>
2020-03-24 10:53:45 -0400

    Create isPalindrome.js

---
+++ isPalindrome.js
@@ -0,0 +1,15 @@
+function isPalindrome(aString){
+
+    //write a reursive function to test if the given string is a isPalindrome
+
+    //use the code form reverse.js to create the reversed aString then see if they are equal
+    //if htis works, rewrite reverse as a pure (no wrapper function) recursive function
+
+}
+
+/test cases:
+console.log(isPalindrome('awesome')); //false
+console.log(isPalindrome('foobar')); //false
+console.log(isPalindrome('tacocat')); //true
+console.log(isPalindrome('amanaplanacanalpanama')); //true
+console.log(isPalindrome('amanaplanacanalpandemonium')); //false
