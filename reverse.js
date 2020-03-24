544766d6c5d458a668109dc8738da2803f81dbcf
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-24 10:43:05 -0400

    reverse works

--- reverse.js
+++ reverse.js
@@ -5,35 +5,23 @@ function reverse(aString){
     let reversedArray = []; //an array to hold the letters as I accumulate them
 
     function reverseInner(aString){
-        console.log("aString is ", aString);
 
         if(aString.length == 1){    //base case
-            reversedArray.push(aString[0]);
-            console.log("reversedArray(base) is ", reversedArray);
-            return reversedArray;
+            return reversedArray.push(aString[0]);
         }
-
-        //string is ewa; shorten the string to ew
+        
+        //recursive
         let aNewString = aString.substring(0, aString.length - 1);
-          //call inner on ew
-          reverseInner(aNewString);
-          //string is ew; shorten to e
-            //call inner on e
-              //this is the base case
-              //return reversedArray
-          //add the last letter of ew to the beginning of reversedArray
-          //return reversedArray
-        //add the last letter of ewa to reversedArray and return reversedArray
+        reverseInner(aNewString);          
         return reversedArray.unshift(aString[aString.length - 1]);
+
     }//end reverseInner()
 
     reverseInner(aString);
 
-    let myOutput = reversedArray.join("");
-    console.log(myOutput);
-    return myOutput ;
+    return reversedArray.join("");
         
 }//end reverse()
 
 console.log(reverse('ewa')); //awesome
-//console.log(reverse('rithmschool')); //loohcsmhtir
+console.log(reverse('rithmschool')); //loohcsmhtir
