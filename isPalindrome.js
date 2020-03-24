fd4b066eca6d9cfaf4257e1e916427106ec63fd3
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-24 11:10:21 -0400

    now change reverse.js to pure recursion

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,13 +1,31 @@
-function isPalindrome(aString){
+function reverse(aString){
+    let reversedArray = [];
+    function reverseInner(aString){
+        if(aString.length == 1){
+            return reversedArray.push(aString[0]);
+        }
+        let aNewString = aString.substring(0, aString.length - 1);
+        reverseInner(aNewString);          
+        return reversedArray.unshift(aString[aString.length - 1]);
+    }
+    reverseInner(aString);
+    return reversedArray.join("");
+}//end reverse()
 
-    //write a reursive function to test if the given string is a isPalindrome
+
+function isPalindrome(anotherString){
+
+    //write a reursive function to test if the given string is a palindrome
+    let aReversedString = reverse(anotherString);
+
+    return (aReversedString == anotherString) ? 'true' : 'false';
 
     //use the code form reverse.js to create the reversed aString then see if they are equal
-    //if htis works, rewrite reverse as a pure (no wrapper function) recursive function
+    //if this works, rewrite reverse as a pure (no wrapper function) recursive function
 
-}
+}//end isPalindrome()
 
-/test cases:
+//test cases:
 console.log(isPalindrome('awesome')); //false
 console.log(isPalindrome('foobar')); //false
 console.log(isPalindrome('tacocat')); //true
