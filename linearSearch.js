d995b9d41f5e4a8e9c8f918076b8ceaa206c35dd
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-25 09:28:07 -0400

    works

--- linearSearch.js
+++ linearSearch.js
@@ -1,21 +1,20 @@
-
 function linearSearch(arr, val){
 
     /* find the value in the array and return it's index
-         or return -1 if value is not an element
-    */
+         or return -1 if value is not an element */
 
     let len = arr.length;
-    for(i = 0; i < len; i ++){
+    for(let i = 0; i < len; i ++){
         if(arr[i] == val){
             return i;
         }
-    }
+    }//end for i
     return -1;
 }//end introToLinearSearch()
 
 console.log(linearSearch(['ebeneezer', 'bodhi', 'bella', 'murphy', 'elwood'], 'bella'));//2
-console.log(linearSearch(['ebeneezer', 'bodhi', 'bella', 'murphy', 'elwood'], 'rick'));//-1linearSearch([10, 15, 20, 25, 30], 15);//1
+console.log(linearSearch(['ebeneezer', 'bodhi', 'bella', 'murphy', 'elwood'], 'rick'));//-1
+console.log(linearSearch([10, 15, 20, 25, 30], 15));//1
 console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4));//5
 console.log(linearSearch([100], 100));//0
 console.log(linearSearch([1, 2, 3, 4, 5], 6));//-1
