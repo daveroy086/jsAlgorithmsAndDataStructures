7824b83f13baf50f3c793a01435891196979ab23
dave086 <51098204+daveroy086@users.noreply.github.com>
2020-03-26 10:47:12 -0400

    Create naiveStringSearch.js

---
+++ naiveStringSearch.js
@@ -0,0 +1,16 @@
+function naiveStringSearch(str1, str2){
+
+    /*loop over the longer string
+      loop over the shorted string
+      if the characters don't match, break out of the inned loop
+      if the characters do match, keep going
+      if you complete the inner loop and find a match,
+      increment the count of matchs
+      return the count of matches
+      */
+
+
+}
+
+console.log(naiveStringSearcha("lorie loled", lol));//1
+console.log(naiveStringSearch("wowomgzomg", omg)); //2
