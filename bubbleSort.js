4b1255a598f1ef0328d374e08e9ad503a12d3ae2
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-27 12:38:51 -0400

    works

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,6 @@
 function bubbleSort(anotherArray){
 
-    console.log(anotherArray);
-    console.log("");
-    
-    /*
-        psueodcode:
-          start looping from with a variable called i theend of the array towards the beginning
-          start the inner loop with a variable called j from the beginning to i - 1
-          if arr[j] is greater the arr[j + 1], swap those two values!
-          return the sorted array
-    */
+    // use the bubble sort algorithm to sort an array of numbers
 
     let aFlag = false;
     let range = anotherArray.length;
@@ -17,24 +8,20 @@ function bubbleSort(anotherArray){
         aFlag = true;
         for(i = 0; i < range; i ++){
             if(anotherArray[i] > anotherArray[i + 1]){
-                 let temp = anotherArray[i];
+                let temp = anotherArray[i];
                 anotherArray[i] = anotherArray[i + 1];
                 anotherArray[i + 1] = temp;
-                //console.log("i is ", i, " and anotherArray is ", anotherArray);
                 aFlag = false;
                 }
-
         }//end for i
         range--;
     }//end while
-    console.log("");
 
     return anotherArray;
 
 }//end bubbleSort()
 
 let anArray = [];
-
 for(k = 0; k < 20; k++){
     anArray.push(Math.floor(Math.random() * 100));
 }
