318074908efb8fab51dbb1ca5a1aed0c199bf506
dave086 <51098204+daveroy086@users.noreply.github.com>
2020-03-31 10:26:14 -0400

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,8 @@
+function insertionSort(arr){
+
+
+}
+
+start by picking the second element in the array
+now compare the second element to the one before it and swap if necessary
+continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place
