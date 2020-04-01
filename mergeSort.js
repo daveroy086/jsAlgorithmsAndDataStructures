e2cd7e606c7b0796e427cf409ac4477c5324f141
dave086 <51098204+daveroy086@users.noreply.github.com>
2020-04-01 13:29:38 -0400

    Create mergeSort.js

---
+++ mergeSort.js
@@ -0,0 +1,5 @@
+function mergeSort(anArray){
+    Pseudocode:
+Break up the array into halves until you have that are empty or have one element
+Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+Once the array has been merged back together, return the merged (and sorted!) array
