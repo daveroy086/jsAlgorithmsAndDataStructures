cb398d9d255f71146c559e20a7d377de3c041706
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-04-10 15:36:53 -0400

    finished his version of quicksort

--- quickSort.js
+++ quickSort.js
@@ -1,41 +1,28 @@
-function swap(anArray, a, b){
-    let temp = anArray[a];
-    anArray[a] = anArray[b];
-    anArray[b] = temp;
+function swap(arr, a, b){
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 }
 
-function pivot(arr, start = 0, end = arr.length + 1){
-
-    //arrPivot = [arr[0], arr[Math.floor(arr.length / 2)], arr[arr.length - 1]];
+function pivot(arr, start = 0, end = arr.length - 1){    
     let pivot = arr[start];
-    let target = 0;
-  
-    for(i = start + 1; i < arr.length; i++){
-        if(arr[i] <= pivot){    // moves smaller values to front of array after pivot
-            swap(arr, i,  target + 1);
-            target++;
+    let swapIndex = start;
+
+    for(let i = start + 1; i <= end; i++){
+        if(pivot > arr[i]){    // moves smaller values to front of array after pivot
+            swapIndex++;
+            swap(arr, swapIndex, i);
         }
     }//end for i
-
-    swap(arr, 0, target);
-    console.log(arr);
-    return target;
+    swap(arr, start, swapIndex);
+    return swapIndex;
 }//end pivot()
 
-function quickSort(aNFA){    //aNFA = anotherNameForArray
-    
-    //Psuedocode:
-    //  call pivot on the array
-    //  when pivot returns the pivot index,recursively call pivot on the subarray to the left 
-    //  that index, and the subarray to the right of that index
-    //  your base case occurs when you have a subarray of less then two elements
-    
-
-}
-
-anotherArray = [];  
-for(i = 0; i < 9; i++){
-    anotherArray.push(Math.floor(Math.random() * 10));
-}
-console.log("anotherArray is ", anotherArray);
-console.log(pivot(anotherArray));
+function quickSort(arr, left = 0, right = arr.length - 1){
+    if(left < right){    //his base case test
+        let pivotIndex = (pivot(arr, left, right));//  call pivot on the arr    
+        quickSort(arr, left, pivotIndex - 1);    //left half
+        quickSort(arr, pivotIndex + 1, right);    //right half
+    }// end if
+    return arr;
+}//end quickSort()
