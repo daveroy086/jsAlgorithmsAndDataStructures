ecb7c77b7bf9199ea047d1e70176a96a236a7205
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-04-12 07:09:04 -0400

    dunno

--- quickSort.js
+++ quickSort.js
@@ -1,28 +1,62 @@
-function swap(arr, a, b){
-    let temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
+function swap(anArray, a, b){
+    let temp = anArray[a];
+    anArray[a] = anArray[b];
+    anArray[b] = temp;
 }
 
-function pivot(arr, start = 0, end = arr.length - 1){    
+function pivot(arr, left, right){
     let pivot = arr[start];
-    let swapIndex = start;
-
-    for(let i = start + 1; i <= end; i++){
-        if(pivot > arr[i]){    // moves smaller values to front of array after pivot
-            swapIndex++;
-            swap(arr, swapIndex, i);
+    let target = 0;
+    for(i = start + 1; i < arr.length; i++){
+        if(arr[i] <= pivot){    // moves smaller values to front of array after pivot
+            swap(arr, i,  target + 1);
+            target++;
         }
     }//end for i
-    swap(arr, start, swapIndex);
-    return swapIndex;
+
+    swap(arr, 0, target);
+    console.log("returned from pivot ", arr);
+    return target;
 }//end pivot()
 
-function quickSort(arr, left = 0, right = arr.length - 1){
-    if(left < right){    //his base case test
-        let pivotIndex = (pivot(arr, left, right));//  call pivot on the arr    
-        quickSort(arr, left, pivotIndex - 1);    //left half
-        quickSort(arr, pivotIndex + 1, right);    //right half
-    }// end if
-    return arr;
+function quickSort(anotherArray, left = 0, right = anotherArray.length - 1){
+    console.log([].concat([1]));
+    console.log("parameter is ", anotherArray);
+    
+    if(anotherArray.length < 2){
+        return anotherArray;
+    }
+    if (anotherArray.length == 0){
+        return;
+    }
+    
+    //Psuedocode:
+    //  call pivot on the array
+    let index = (pivot(anotherArray, left, right));
+   
+    if (index === 0){
+        index = 1;
+    }
+    let indexArray = [index];
+    console.log("index is ", index);
+    let lesser = anotherArray.slice(0, index);
+    console.log("lesser is ", lesser);
+    let greater = anotherArray.slice(index + 1);
+    console.log("greater is ", greater);
+    let myAns = quickSort(anotherArray.slice(0, index))
+                   .concat(indexArray
+                       .concat(quickSort(anotherArray.slice(index + 1))));
+    console.log("returned is ", myAns);
+    return myAns;
+    //  that index, and the subarray to the right of that index
+    //  your base case occurs when you have a subarray of less then two elements
+    
 }//end quickSort()
+
+testArray = [5, 1, 2, 5, 3, 8, 1];  
+/* for(i = 0; i < 6; i++){
+    testArray.push(Math.floor(Math.random() * 10));
+} */
+//console.log("testArray is ", testArray);
+console.log(quickSort(testArray));
+//getMedian = [arr[0], arr[Math.floor(arr.length / 2)], arr[arr.length - 1]];
