6517faee05a4c739055acbf0234dfb8a347ad235
dave086 <51098204+daveroy086@users.noreply.github.com>
2020-04-22 13:16:46 -0400

    Create stack.js

---
+++ stack.js
@@ -0,0 +1,9 @@
+// Create two functions that implement:
+    // Stack with SLL
+    // Queue with SLL
+
+  // Notes:
+      // Make notes about implementing each with an array...two or three lines about each
+          Why is the SLL version better
+      // Pay attention to how the push, pop and peek functions included in the SLL versions
+           are different fromt the original SLL versions
