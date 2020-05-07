6f0363fa4257adb28ed964e7ae2ff7f6e15d685d
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-05-07 10:34:36 -0400

    has remove psuedocode

--- binaryHeap.js
+++ binaryHeap.js
@@ -16,5 +16,19 @@ class BinaryHeap {    // This is a Max binary heap
         }   // end while
         return this.anArray;
     }    // end insert()
+
+    remove() {
+        // pseudocode:
+        // Swap the first value in the anArray property with the last one
+        // Pop from the anArray property so that you cna return it at the end
+        // Have the new root "sink down" to the correct spot
+            // Your parent index starts at 0
+            // Find the index of the left child: (2 * index) +1 (make sure it is not out of bounds)
+            // Finid the index of the right child: (2 * index) + 1 (make sure...)
+            // If the left or right element is greater, swap...if both are larger swap with the largest one
+            // The child index you swapped to now becomes the new parent index
+            // Keep looping and swapping until neither child is larger then the parent
+
+    }
     
 }    // end BinaryHeap()
