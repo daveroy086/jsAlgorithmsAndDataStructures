c16214b3d4c58f1728925cf5b1627858b6a3af2e
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-05-20 18:32:40 -0400

    hash and set work

--- HashTable.js
+++ HashTable.js
@@ -1,15 +1,16 @@
-class HashTable {
-    constructor(size = 53){
+                            class HashTable {
+    constructor(size = 17){
         this.keyMap = new Array(size);
     }
 
     _hash(key) {
         let total  = 0;
         let WEIRD_PRIME = 31;
-        for(let I = 0; i < Math.min(key.length, 100); i++) {
+        console.log(key.length);
+        for(let i = 0; i < Math.min(key.length, 100); i++) {
             let char = key[i];
             let value = char.charCodeAt(0) - 96;
-            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+            total = (total * WEIRD_PRIME + value) %  this.keyMap.length;
         }
         return total;
     }
@@ -18,13 +19,16 @@ class HashTable {
     // pseudocode:
         // set:
     set(key, value) {    // accepts a key / value pair
-        let someHash = this._hash(value);    // hashes the key
-        // stores the keyvalue pair in the hash table array via seperate chaining
-        keyMap[someHash].push([key, value]);
-        /***************************use deleteMe to finish making this work */
+        let aHash = this._hash(key);    // hashes the key
+        if(this.keyMap[aHash]){    // stores the keyvalue pair in the hash table array via seperate chaining
+            this.keyMap[aHash].push([key, value]);
+        } else {
+            this.keyMap[aHash] = [[key, value]];
+        }
+
     }
         // get:
-    get(key) {    // acccepts a key
+    get(key) {    // accepts a key
         let moreHash = this._hash(value);    // hashes the key
         if(keyMap[moreHash]) {
             return anArray[moreHash];    // retrieves the key - value pair in the hash table
@@ -33,3 +37,10 @@ class HashTable {
         }
     }
 }// end HashTable
+
+let ht = new HashTable();
+ht.set("abc", "def");
+console.log(ht);
+console.log("");
+ht.set("abc", "ghi");
+console.log(ht);
