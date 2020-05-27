929f101b0505dd40d98cc5fb377b1ba8176ea92a
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-05-27 17:11:22 -0400

    includes addVertex() pseudocode

--- Graph.js
+++ Graph.js
@@ -1,5 +1,11 @@
-class Graph {
+class Graph {    // tis wll be an undirected graph
     constructor() {
         this.adjacencyList = {};
+    }
+
+    addVertex(name) {
+        //pseudocode:
+        //write a method called addVertex, which accepts a name of a vertex
+        //It should add a key to the adjacency listwith the name of the vertex and set it's value to be an empty array
     }
 }
