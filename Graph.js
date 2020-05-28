dde153ae64fbe2a29600749d6ed44bdc6f3af1f8
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-05-28 08:07:07 -0400

    my version of addEdge works

--- Graph.js
+++ Graph.js
@@ -1,16 +1,45 @@
 class Graph {    // tis wll be an undirected graph
     constructor() {
-        this.adjacencyList = {};
+        this.adjacencyList = {"Tokyo" : [],
+                              "Dallas": [],
+                              "Aspen": []};
     }
 
-    addVertex(name) {
-        //pseudocode:
-        //write a method called addVertex, which accepts a name of a vertex
-        //It should add a key to the adjacency listwith the name of the vertex and set it's value to be an empty array
-        this.adjacencyList[name] = [];
+    addVertex(vertex) {
+        // pseudocode:
+        // write a method called addVertex, which accepts a name of a vertex
+        // It should add a key to the adjacency list with the name of the vertex and set it's value to be an empty array
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+        // the if prevents you from overwritinvertexsg an existing vertex...what 'else' could you add too have both ...can you have both
+    }
+
+    addEdge(vertex1, vertex2) {
+        // pseudocode:
+        // this function should accept two vertices named vertex1 and vertex2
+            // loop thru adjacencyList
+           // for(let i = 0; i < Object.keys(this.adjacencyList).length; i++) {
+        // The function should find in the adjacencyList the key of vertex1 and push vertex2 to the array
+            // if adjacencyList.key == vertex1 adjacencyList.vertex1 = vertex2
+           // if(this.adjacencyList[])
+        // The function should find in the adjacencyList the key of vertex2 and push vertex1 to the array
+            // if adjacencyList.key == vertex2 adjacencyList.vertex1 = vertex1
+            Object.keys(this.adjacencyList).forEach((key) => {
+                if(key == vertex1) this.adjacencyList[vertex1].push(vertex2);
+                if(key == vertex2) this.adjacencyList[vertex2].push(vertex1);
+            });
+            /**Sometimes when I write out what is wrong with my code the correct answer comes to me sooner??? */
+        // we're nor worried about error handling / invalid vertices at this time
     }
 }
 
 let g = new Graph();
-g.addVertex("Tokyo");
-console.log(g);
+// test addVertex()
+// g.addVertex("Tokyo");
+// console.log(g);
+
+// test addEdge():
+console.log("The graph is ", g);
+g.addEdge("Tokyo", "Dallas");
+console.log("The graph is ", g);
+g.addEdge("Dallas", "Aspen");
+console.log("The graph is ", g);
