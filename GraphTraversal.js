9fc3075c60216e21dcce26fdd6bd844b3b55b5e7
dave086 <51098204+daveroy086@users.noreply.github.com>
2020-05-31 06:39:17 -0400

    Create GraphTraversal.js

---
+++ GraphTraversal.js
@@ -0,0 +1,27 @@
+class Graph {    // this is an undirected, unweughted graph
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(anElementOfAdjacencyList => anElementOfAdjacencyList != vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(anElementOfAdjacencyList => anElementOfAdjacencyList != vertex1);
+    }
+
+    removeVertex(v) {
+        var self = this;
+        this.adjacencyList[v].forEach(function(anElemOfAL){
+            self.removeEdge(v, anElemOfAL);
+        });
+        delete this.adjacencyList[v];
+    }
+}    // end Graph
