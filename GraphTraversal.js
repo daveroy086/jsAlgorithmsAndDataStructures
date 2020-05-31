0c45cfabd31a00be519630e447f9d3b22b7bc297
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-05-31 07:18:27 -0400

    has recursiveDFS pseudocode

--- GraphTraversal.js
+++ GraphTraversal.js
@@ -24,4 +24,32 @@ class Graph {    // this is an undirected, unweughted graph
         });
         delete this.adjacencyList[v];
     }
+
+    recursiveDFS(vrtx) {
+        // pseudocode:
+        // The function should accept a starting node
+        // Create a list to store the end result, to be returned at the end
+        // Create an object to store visited vertices
+        // Create a helper function that accepts a vertex
+            // The helper function should return early (empty?) if the vertex is empty (has been visited?)
+            // The helper function should place the vertex it accepts into the visited object and push that vertex into the results array
+            // Loop over all of the values in the adjacencyList for that vertex
+            // If any of those values have not been visited, recursivley invoke the helper function with that vertex
+    }
 }    // end Graph
+
+let g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
