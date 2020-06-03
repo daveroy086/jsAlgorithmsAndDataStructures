9824e6f0361771d9a1c0f0f18284901974c4c743
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-06-03 14:21:59 -0400

    has recursiveDFS

--- GraphTraversal.js
+++ GraphTraversal.js
@@ -25,27 +25,45 @@ class Graph {    // this is an undirected, unweughted graph
         delete this.adjacencyList[v];
     }
 
-    recursiveDFS(vrtx) {
+    recursiveDFS(start) {
+        let that = this;
         // pseudocode:
         // The function should accept a starting node
         // Create a list to store the end result, to be returned at the end
+        let toReturn = [];
         // Create an object to store visited vertices
+        let visited = {};
         // Create a helper function that accepts a vertex
-            // The helper function should return early (empty?) if the vertex is empty (has been visited?)
+        function helper(vertex) {    // The helper function should return early (empty?) if the vertex is 
+                               //empty (has been visited?)
+             if(!vertex) return null;    //base case
             // The helper function should place the vertex it accepts into the visited object and push that vertex into the results array
+            //let that = this;
+            visited[vertex] = "true";
+            console.log("visited is ", visited);
+            toReturn.push(vertex);
+            console.log("toReturn is ", toReturn);
             // Loop over all of the values in the adjacencyList for that vertex
+            that.adjacencyList[vertex].forEach(function(anotherElemOfAL){
+                if(!toReturn.includes(anotherElemOfAL)) helper(anotherElemOfAL);
+            });
             // If any of those values have not been visited, recursivley invoke the helper function with that vertex
-    }
+        }    // end helper()
+        helper(start);
+        return toReturn;
+    }    // end recursiveDFS()
 }    // end Graph
 
 let g = new Graph();
 
+// for testing recursiveDFS():
 g.addVertex("A");
 g.addVertex("B");
 g.addVertex("C");
 g.addVertex("D");
 g.addVertex("E");
 g.addVertex("F");
+console.log(g);
 g.addEdge("A", "B");
 g.addEdge("A", "C");
 g.addEdge("B", "D");
@@ -53,3 +71,7 @@ g.addEdge("C", "E");
 g.addEdge("D", "E");
 g.addEdge("D", "F");
 g.addEdge("E", "F");
+console.log("Now, g is ", g);
+
+console.log(g.recursiveDFS("A"));//[ 'A', 'B', 'D', 'E', 'C', 'F' ]
+//say my name
