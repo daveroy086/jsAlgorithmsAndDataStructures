6583f9389d4fc117493b6d3eea62fec2cce0489b
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-07-16 14:37:06 -0400

    works

--- Exercises35through85/17someReursive.js
+++ Exercises35through85/17someReursive.js
@@ -1,4 +1,18 @@
-function someRecursive(arr, aCallback){
+ function someRecursive (arr, callback) {
+    if (arr.length == 1) {    // base case
+       return callback(arr[0]);
+    }
+    return callback(arr[0]) || someRecursive(arr.slice(1), callback) ? true : false;
+    
+} // end someRecursive()
+
+const isOdd = val => val % 2 !== 0;
+console.log(someRecursive([1,2,3,4], isOdd)); // true
+console.log(someRecursive([4,6,8,9], isOdd)); // true
+console.log(someRecursive([4,6,8], isOdd)); // false}
+console.log(someRecursive([4,6,8], val => val > 10)); // false
+
+/* function someRecursive(arr, aCallback){
 
     //fix the name of the file
 
@@ -9,14 +23,30 @@ function someRecursive(arr, aCallback){
     //test the first element of the array and call someRecursive
     // on the subarray that doesn't contain the first element
 } // end 17someRecursive
+ */
 
-// Tests:
-const isOdd = val => val % 2 !== 0;
-console.log(someRecursive([1,2,3,4], isOdd)); // true
-console.log(someRecursive([4,6,8,9], isOdd)); // true
-console.log(someRecursive([4,6,8], isOdd)); // false
-console.log(someRecursive([4,6,8], val => val > 10)); // false
+/*
+ Without the callback
+ function someRecursive (arr) {
+    if (arr.length == 1) {    // base case
+        return arr[0] % 2 !==0 ? true : false;
+    }
+    return arr[0] % 2 !== 0 someRecursive(arr.slice(1)) ? true : false;
+} // end someRecursive()
+ */
+
 /* 
 Instructions:
 
 Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false. */
+
+/* 
+function anotherFunction() {
+    console.log("Hello", anotherVar);
+}
+function aFunctionWithACallback(aVar, aFunction){
+    aFunction();
+}
+let anotherVar = "world";
+anotherFunction(anotherVar, anotherFunction);
+ */
