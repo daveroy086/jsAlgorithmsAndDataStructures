7e6305be5de1a6bd7ecd5cb90405effd594a6988
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-07-17 15:50:40 -0400

    done

--- Exercises35through85/19capitalizeFirst.js
+++ Exercises35through85/19capitalizeFirst.js
@@ -1,12 +1,16 @@
-function capitalizeFirst () {
+function capitalizeFirst (arr) {
 
     //Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
-    // llop thru the array
-    // get the first word, capitalize it and add it to the output Array
-    // call the function on the remaining subarry...capitalizeFirst(arr.slice(1))
-    //this is veru similar to a prob one or two back
-
-}
+    if (arr.length == 1) {    // base case
+        let output = [];
+        output.push(arr[0][0].toUpperCase() + arr[0].slice(1));
+        return output;
+        //yu can also do 'return [arr[0][0].toUpperCase() + arr[0].slice(1)];', but it's less clear
+    }
+    let aWord = [];
+    aWord.push(arr[0][0].toUpperCase() + arr[0].slice(1));
+    return aWord.concat(capitalizeFirst(arr.slice(1)));
+} // end capitalizeFirst()
 
 // Test:
 console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
