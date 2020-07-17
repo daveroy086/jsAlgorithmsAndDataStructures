03ec094fe2e6e7bee646e3d8fbca0b1a8c372191
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-07-17 09:36:30 -0400

    19 contains pseudocode

---
+++ Exercises35through85/19capitalizeFirst.js
@@ -0,0 +1,12 @@
+function capitalizeFirst () {
+
+    //Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+    // llop thru the array
+    // get the first word, capitalize it and add it to the output Array
+    // call the function on the remaining subarry...capitalizeFirst(arr.slice(1))
+    //this is veru similar to a prob one or two back
+
+}
+
+// Test:
+console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
