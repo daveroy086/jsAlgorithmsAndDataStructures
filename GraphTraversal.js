ce39d8a687a67b733223b07c7776b4d9827079cb
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-07-17 09:29:06 -0400

    needs work

--- GraphTraversal.js
+++ GraphTraversal.js
@@ -78,7 +78,7 @@ class Graph {    // this is an undirected, unweughted graph
         // Loop as long as there is anything in the queue (while)
         while(queue.length) {
             // Remove the first vertex from the queue and push it into the array that stores the nodes visited (list)
-            let newVertex = queue.shift();
+            let newVertex = queue.shift();    // oops********************************
             list.push(newVertex);
             // Loop over each vertex in the adjacency list for the vertex you are visiting
             // If it is not inside the object that stores the nodes visited, mark it as visited and enqueue that vertex
