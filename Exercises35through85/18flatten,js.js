694c9c611a0b125dd4e3fb2acfc1187d68d220d6
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-07-17 09:27:39 -0400

    done

--- Exercises35through85/18flatten,js.js
+++ Exercises35through85/18flatten,js.js
@@ -1,14 +1,23 @@
-function flatten(){
+function flatten(arr){
 
-    // create an output array
+    let len = arr.length;
+    let outputArray = [];
     // loop thru the array
-    // if an element is an integer push it into the output array
-    // if the element is an array call flatten on it
-}
+    for (let i = 0; i < len; i++) {
+        if(typeof arr[i] == "object") {    // if the element is an array call flatten on it
+            outputArray = outputArray.concat(flatten(arr[i]));
+        } else {    // if an element is an integer push it into the output array
+            outputArray.push(arr[i]);
+        }
+    }
+    return outputArray;
+} // end flatten()
 
 console.log(flatten([1, 2, 3, [4, 5] ])); // [1, 2, 3, 4, 5]
 console.log(flatten([1, [2, [3, 4], [[5]]]]) );// [1, 2, 3, 4, 5]
 console.log(flatten([[1],[2],[3]])); // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
+
 /* 
-Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened. */
+Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
+*/
