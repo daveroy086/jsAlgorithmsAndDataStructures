752d8a8c967085e2f48df961266c9e91e6aeddee
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-08-03 08:58:55 -0400

    shortened var names

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,32 +1,34 @@
-function mergeArrays(array1, array2){    //merge two sorted arrays
+function mergeArrays(arr1, arr2){    //merge two sorted arrays
 
     let output = [];
     let i = 0;//first array index
     let j = 0;//second array index
-    let len1 = array1.length;
-    let len2 = array2.length;
+    let len1 = arr1.length;
+    let len2 = arr2.length;
 
     while((i + j) < (len1 + len2)){
-        if(array1[i] < array2[j]){
-          output.push(array1[i]);
+        if(arr1[i] < arr2[j]){
+          output.push(arr1[i]);
           i++;
           if( i == len1){
-            output = output.concat(array2.splice(j));
-            i = i + array2.slice(j).length;
+            output = output.concat(arr2.splice(j));
+            i = i + arr2.slice(j).length;
             return output;    
           }
         }else{
-          output.push(array2[j]);
+          output.push(arr2[j]);
           j++;
           if( j == len2){
-            output = output.concat(array1.slice(i));
-            j = j + array1.slice(i).length;
+            output = output.concat(arr1.slice(i));
+            j = j + arr1.slice(i).length;
             return output;    
           }
         }//end else
     }//end while()
 }//end mergeArrays()
 
+
+console.log(mergeArrays([1, 10, 50], [2, 14, 99, 100]));s
 /*//to test:
 function insertionSort(arr){
 
@@ -57,15 +59,15 @@ function makeSortedArray(){
 return insertionSort(anArray);
 }// end makeSortedArray()
 
-//anotherArray1 = [ 2, 3, 6, 7, 8];
-anotherArray1 = makeSortedArray();
-console.log("anotherArray1 is ", anotherArray1);
+//anotherarr1 = [ 2, 3, 6, 7, 8];
+anotherarr1 = makeSortedArray();
+console.log("anotherarr1 is ", anotherarr1);
 
-//anotherArray2 = [ 0, 1, 4, 5, 9, 10, 11, 12];
-anotherArray2 = makeSortedArray();
-console.log("anotherArray2 is ", anotherArray2);
+//anotherarr2 = [ 0, 1, 4, 5, 9, 10, 11, 12];
+anotherarr2 = makeSortedArray();
+console.log("anotherarr2 is ", anotherarr2);
 
-console.log(mergeArrays(anotherArray1, anotherArray2));
+console.log(mergeArrays(anotherarr1, anotherarr2));
 */
 
 /*
