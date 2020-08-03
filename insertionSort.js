6b2492559721a04c78655b63ad710c326c094bda
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-08-03 06:03:35 -0400

    includes his code

--- insertionSort.js
+++ insertionSort.js
@@ -18,6 +18,7 @@ function insertionSort(arr){
     }//end for i
     return arr;
 }//end insertionSort()
+//his code is below
 
 for(m = 0; m < 10; m ++){
     let anArray = [];
@@ -34,3 +35,27 @@ start by picking the second element in the array
 now compare the second element to the one before it and swap if necessary
 continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place
  */
+
+/*
+//his insertionSort
+function insertionSort(arr) {
+    console.log(arr);
+    console.log("");
+
+    let arrLength = arr.length;
+    for(let i = 1; i < arrLength; i++) {//loop thru the array staring with the second element
+    console.log("i is ", i);
+    let currentVal = arr[i];
+        for(var j = i - 1; j > -1 &&  arr[j] > currentVal; j--) {
+            arr[j + 1] = arr[j]; //shift each element up an index untll you get down to where
+            console.log(arr);         //currentVal belongs
+        }
+        arr[j + 1] = currentVal; // then insert currentVal
+        console.log(arr);
+        console.log("");
+    } // end for  i
+    return arr;
+} // end insertion sort
+
+insertionSort([10, 8, 6, 7, 15, 4, 16, 22, 2]);
+*/
