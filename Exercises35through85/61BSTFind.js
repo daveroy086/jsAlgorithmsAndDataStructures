8039cc3f0bf974d6ff2ba09c9bd32c6ed3f18fc5
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-08-26 03:56:00 -0400

    has comments, needs more

--- Exercises35through85/61BSTFind.js
+++ Exercises35through85/61BSTFind.js
@@ -1,15 +1,4 @@
-//exercise 61
-//BinarySearchTree - find
-
-/*Instructions:
-Implement the following functions on the `BinarySearchTree.prototype`. insert has been implemented for you to help with testing.
-
-findIteratively
-
-This function should find a node in a binary tree. It should return the node if found, otherwise return `undefined`. This should be solved using iteration or recursion. The tests for this method assume that insertIteratively has been implemented correctly.
-*/
-
-class Node {
+class Node {    //class Node, class BinarySearchTree and find() are CS's code
   constructor(value) {
     this.value = value;
     this.left = null;
@@ -24,7 +13,6 @@ class BinarySearchTree {
   insert(value) {
     if (this.root === null) {
       this.root = new Node(value);
-      console.log("Am I here?");
       return this;
     } else {
       var current = this.root;
@@ -46,17 +34,17 @@ class BinarySearchTree {
         }
       }
     }
-  }
+  }  //  end insert
 
-    find(value1) {
-        if(this.root == null) {
-           return false;
+    find(value1) {    // search tre for node with value1
+        if(this.root == null) {    //  if tree has node nodes
+           return undefined;
         } else {
-            let current = this.root;
+            let current = this.root;    //  examine root
             while(current){
-
-                if(current.value == value1) return current;
-
+                if(current.value == value1) {
+                    return current;
+                }
                 if(value1 < current.value){
                     if(current.left == null) return undefined;
                     if(current.left.value == value1) {
@@ -65,7 +53,6 @@ class BinarySearchTree {
                         current = current.left;
                     }
                 }
-
                 if(current.value < value1){
                     if(current.right == null) return undefined;
                     if(current.right.value == value1) {
@@ -74,10 +61,10 @@ class BinarySearchTree {
                         current = current.right;
                     }
                 }
-
             } // end while
         } // end else
-    }    // end find   
+    }    // ends find
+
 }
 
 //tests:
@@ -88,10 +75,10 @@ binarySearchTree
     .insert(10)
     .insert(12);
 var foundNode = binarySearchTree.find(20);
-console.log(foundNode.value); // 20
-console.log(foundNode.left); // null
-console.log(foundNode.right); // null
-    
+console.log("the foundNodes value is: ", foundNode.value); // 20
+console.log("the foundNodes left child node is: ", foundNode.left); // null
+console.log("the foundNodes right child node is: ", foundNode.right); // null
+/*     
 var binarySearchTree = new BinarySearchTree();
     
 binarySearchTree
@@ -101,3 +88,10 @@ binarySearchTree
     .insert(12);
 var foundNode = binarySearchTree.find(120);
 foundNode // undefined
+ *//*Instructions:
+Implement the following functions on the `BinarySearchTree.prototype`. insert has been implemented for you to help with testing.
+
+findIteratively
+
+This function should find a node in a binary tree. It should return the node if found, otherwise return `undefined`. This should be solved using iteration or recursion. The tests for this method assume that insertIteratively has been implemented correctly.
+*/
