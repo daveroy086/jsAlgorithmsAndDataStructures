9273f2200503ee6f69669c435473933687c91029
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-08-26 03:58:51 -0400

    removed comments

--- Exercises35through85/61BSTFind.js
+++ Exercises35through85/61BSTFind.js
@@ -36,11 +36,11 @@ class BinarySearchTree {
     }
   }  //  end insert
 
-    find(value1) {    // search tre for node with value1
-        if(this.root == null) {    //  if tree has node nodes
+    find(value1) {    // search tree for node with value1
+        if(this.root == null) {
            return undefined;
         } else {
-            let current = this.root;    //  examine root
+            let current = this.root;
             while(current){
                 if(current.value == value1) {
                     return current;
@@ -61,37 +61,8 @@ class BinarySearchTree {
                         current = current.right;
                     }
                 }
-            } // end while
-        } // end else
+            }    // end while
+        }    // end else
     }    // ends find
 
-}
-
-//tests:
-var binarySearchTree = new BinarySearchTree();
-binarySearchTree
-    .insert(15)
-    .insert(20)
-    .insert(10)
-    .insert(12);
-var foundNode = binarySearchTree.find(20);
-console.log("the foundNodes value is: ", foundNode.value); // 20
-console.log("the foundNodes left child node is: ", foundNode.left); // null
-console.log("the foundNodes right child node is: ", foundNode.right); // null
-/*     
-var binarySearchTree = new BinarySearchTree();
-    
-binarySearchTree
-    .insert(15)
-    .insert(20)
-    .insert(10)
-    .insert(12);
-var foundNode = binarySearchTree.find(120);
-foundNode // undefined
- *//*Instructions:
-Implement the following functions on the `BinarySearchTree.prototype`. insert has been implemented for you to help with testing.
-
-findIteratively
-
-This function should find a node in a binary tree. It should return the node if found, otherwise return `undefined`. This should be solved using iteration or recursion. The tests for this method assume that insertIteratively has been implemented correctly.
-*/
+}    // BinarySearchTree
