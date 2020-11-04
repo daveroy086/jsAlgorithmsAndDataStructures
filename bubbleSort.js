838b55984e4c431615919eb45415358637462289
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-11-04 12:18:53 -0500

    includes his code

--- bubbleSort.js
+++ bubbleSort.js
@@ -29,4 +29,27 @@ for(k = 0; k < 20; k++){
     anArray.push(Math.floor(Math.random() * 100));
 }
 
-console.log(bubbleSort(anArray)); 
+console.log(bubbleSort(anArray));
+/*
+His original pseudocode:
+Start looping from with a varable called i the end of the array toward the beginning
+    St=tart an inner loopwith a variable called j from the beginninguntil i - 1
+    If arr[j] is greater than arr[j + 1] , swap those two values!
+    return thre sorted array
+
+His original code:
+function bubbleSort(arr){
+    for(var i = arr.length; i > 0; i--){
+        for(var j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([37, 45, 29, 8, 12, 88, -3]));
+*/
