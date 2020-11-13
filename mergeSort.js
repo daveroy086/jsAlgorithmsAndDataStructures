10922e4ee284100477aeb5e33f7a7e48a77c62d4
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-11-13 16:24:27 -0500

    no message

--- mergeSort.js
+++ mergeSort.js
@@ -4,7 +4,8 @@ function mergeSort(anArray){
       //see the section on breaking to a label
     
     /* Pseudocode:
-        Break up the array into halves until you have that are empty or have one element
+        Break up the array into halves until all subarays are empty or have only one element 
+             (now each subarray is sorted)
         from audio:
           use .slice() to bisect the array and
           use recursion to get it down to one element arrays
@@ -12,7 +13,8 @@ function mergeSort(anArray){
         Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
         Once the array has been merged back together, return the merged (and sorted!) array
     */
-    let arraylength  = anArray.length;
+
+    let arraylength = anArray.length;
 
     if(arraylength <= 1){    //base case
       return anArray;
@@ -27,26 +29,31 @@ function mergeSort(anArray){
 
 function mergeArrays(array1, array2){    //merge two sorted arrays
     let output = [];
-    let i = 0;
+    let i = 0;   // the number of elements of array1 you've copied to output
     let j = 0;
     let len1 = array1.length;
     let len2 = array2.length;
     while((i + j) < (len1 + len2)){
         if(array1[i] < array2[j]){
-          output.push(array1[i]);
-          i++;    // moves the pointer instead of removing the element?...why? 
-          if( i == len1){    // if you reach the end of a1 concat the rest of a2?
-            output = output.concat(array2.splice(j));    //check the vid
-            i = i + array2.slice(j).length;
-            return output;}
+            output.push(array1[i]);
+            i++; 
+            if( i == len1){    // if you reach the end of a1 concat the rest of a2
+              output = output.concat(array2.splice(j));
+              i = i + array2.slice(j).length;
+              return output;}
         }else{
-          output.push(array2[j]);
-          j++;
-          if( j == len2){
-            output = output.concat(array1.slice(i));
-            j = j + array1.slice(i).length;
-            return output;
-}}}}//console.log(mergeArrays([ 2, 3, 6, 7, 8], [ 0, 1, 4, 5, 9, 10, 11, 12])); 
+            output.push(array2[j]);
+            j++;
+            if( j == len2){
+              output = output.concat(array1.slice(i));
+              j = j + array1.slice(i).length;
+              return output;
+            }
+        }
+    }    // end while
+}    // end mergeArrays
+
+//console.log(mergeArrays([ 2, 3, 6, 7, 8], [ 0, 1, 4, 5, 9, 10, 11, 12])); 
 //check out my improved mergeArrays below
 // also his version
 
@@ -54,7 +61,7 @@ console.log(mergeSort( [8, 4, 7, 1, 6, 3, 5, 2, 9]));
 
 /*Note to self:
 
-    this works:
+    starting with:
     let arraylength  = anArray.length;
 
     if(arraylength <= 1){    //base case
@@ -64,23 +71,20 @@ console.log(mergeSort( [8, 4, 7, 1, 6, 3, 5, 2, 9]));
     let firstHalf = anArray.slice(0, Math.floor(arraylength / 2));
     let secondHalf = anArray.slice(Math.floor(arraylength / 2));
 
+
+    this works:
     output = mergeArrays(mergeSort(firstHalf), mergeSort(secondHalf));
+    // use console.log to see what mergeSort(firstHalf) and  mergeSort(secondHalf)
+    // look like when they are sent to mergeArrays
     return output;
     //the output is correct...[ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 
     this doesn't:
-    let arraylength  = anArray.length;
-
-    if(arraylength <= 1){    //base case
-      return anArray;
-    }
-
-    let firstHalf = anArray.slice(0, Math.floor(arraylength / 2));
-    let secondHalf = anArray.slice(Math.floor(arraylength / 2));
-
     firstSorted = mergeSort(firstHalf);
     secondSorted = mergeSort(secondHalf);
     output = mergeArrays(firstSorted, secondSorted);
+    // use console.log to see what firstSorted and secondSorted
+    // look like when they are sent to mergeArrays
     return output;
     
 */
